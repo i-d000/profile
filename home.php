f783750e28393d2ab4080d599339024b9a211c97
ooitanojohn <qmkh733310@gmail.com>
2022-02-21 19:17:42 +0900

    profile

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+
+<body>
+    <!-- modal -->
+    <form method="post">
+        <label for="pass-word">パスワード</label>
+        <input type="password" id="pass-word" name="password">
+        <label for="e-mail">メールアドレス</label>
+        <input type="mail" id="e-mail" name="email">
+        <label for="comment">コメント</label>
+        <textarea id="comment" name="comment"></textarea>
+    </form>
+    <!-- show -->
+
+</body>
+
+</html>
