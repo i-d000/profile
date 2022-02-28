98e4f12549318de04056d942e7894d460a2201e1
ooitanojohn <qmkh733310@gmail.com>
2022-02-28 17:34:54 +0900

    login画面作った

--- view/login.php
+++ view/login.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="jp">
 
 <head>
     <meta charset="UTF-8">
@@ -17,27 +17,32 @@
                 <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
                 <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
             </div>
-            <form class="mt-8 space-y-6" action="#" method="POST">
+            <form action="" method="post">
+                <ul>
+                    <li>
+                        <label class="loginId">ログインID</label>
+                        <input type="text" name="loginId" placeholder="ohs" value="<?php echo htmlspecialchars($displayId, ENT_QUOTES); ?>"><br>
+                    </li>
+                    <li>
+                        <label class="pass">パスワード</label>
+                        <input type="text" name="pass" placeholder="mode"><br>
+                    </li>
+                    <li>
+                        <button type="submit" name="btn" value="login" id="login">ログイン</button><br>
+                    </li>
+                    <span><?php echo $msg; ?></span>
+                </ul>
+            </form>
+            <form class="mt-8 space-y-6" action="./login.php" method="POST">
                 <input type="hidden" name="remember" value="true">
                 <div class="rounded-md shadow-sm -space-y-px">
                     <div>
                         <label for="email-address" class="sr-only">6桁学籍番号</label>
-                        <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+                        <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Login ">
                     </div>
                     <div>
                         <label for="password" class="sr-only">Password</label>
                         <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
-                    </div>
-                </div>
-
-                <div class="flex items-center justify-between">
-                    <div class="flex items-center">
-                        <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-                        <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
-                    </div>
-
-                    <div class="text-sm">
-                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
                     </div>
                 </div>
 
@@ -55,22 +60,7 @@
             </form>
         </div>
     </div>
-    <form action="./login.php" method="post">
-        <ul>
-            <li>
-                <label class="loginId">ログインID</label>
-                <input type="text" name="loginId" placeholder="ohs" value="<?php echo htmlspecialchars($displayId, ENT_QUOTES); ?>"><br>
-            </li>
-            <li>
-                <label class="pass">パスワード</label>
-                <input type="text" name="pass" placeholder="mode"><br>
-            </li>
-            <li>
-                <button type="submit" name="btn" value="login" id="login">ログイン</button><br>
-            </li>
-            <span><?php echo $msg; ?></span>
-        </ul>
-    </form>
+
 </body>
 
 </html>
