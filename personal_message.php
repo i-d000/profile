5ae9d7fce0559efac7e3301d8bbfe636ddf54d78
Ouki74531 <88080310+Ouki74531@users.noreply.github.com>
2022-03-01 11:56:14 +0900

    Update personal_message.php

--- personal_message.php
+++ personal_message.php
@@ -7,21 +7,21 @@ if(!isset($_GET["id"])){
 }else{
     $partner_id = $_GET["id"];
 }
-
 //メッセージを送る処理
-if(isset($_POST["send"])){
+if(isset($_POST["submit"])){
     //画像が選択されている場合の処理
     $img = "";
-    if($_FILES["file"]["name"] != ""){
-        $upload_file = $_FILES['file'];
+    if(isset($_FILES["file"]["name"])){
+       $upload_file = $_FILES['file'];
         move_uploaded_file($upload_file["tmp_name"] , "./img/".$_FILES['file']['name']);
         $img = $_FILES['file']['name'];
     }
-    $link = mysqli_connect(HOST , USER_ID ,PASSWORD , DB_NAME);
-    mysqli_set_charset($link , 'utf8');
-    $message = mysqli_real_escape_string($link , $_POST["message"]);
-    mysqli_query($link , "INSERT INTO direct_message (text , img ,profile_id , destination_profile_id) 
-                            VALUES('". $message ."', '". $img ."' , ". $_COOKIE["id"] ." , ". $partner_id.")");
+    if($_POST["message"] != "" ){
+        $link = mysqli_connect(HOST , USER_ID ,PASSWORD , DB_NAME);
+        mysqli_set_charset($link , 'utf8');
+        $message = mysqli_real_escape_string($link , $_POST["message"]);
+        mysqli_query($link , "INSERT INTO direct_message(text , img , profile_id , destination_profile_id) VALUES('". $message ."' , '". $img ."' , ". $_COOKIE["id"] ." , ". $partner_id.")");
+    }
 }
 
 //相手とのメッセージを全件取得してくる処理
@@ -46,7 +46,7 @@ mysqli_close($link) ;
 //使用者が相手に送ったメッセージか相手から自分に送られたメッセージかによってクラス分けをする処理
 for($i = 0 ; $i < count($list) ; $i++){
     //画像が存在していた場合
-    if($list[$i]["画像"] != ""){
+    if(isset($list[$i]["画像"])){
         $list[$i]["画像"] = '<p><img src="./img/'. $list[$i]["画像"] .'" alt="画像"';
     }
     //使用者から相手に送ったメッセージ
