e797e98853d9802a8a9443e398e3b769c4f3e81b
OhtaniKoki <ohtani.koki01@gmail.com>
2022-03-01 11:31:09 +0900

    searchを繋げました

--- search.php
+++ search.php
@@ -3,38 +3,34 @@
 include './setting/const.php';
 
 $search = '';
-$error = 'hidden';
 
 if (isset($_POST['search'])) {
   $search = $_POST['search'];
 }
 
-//ああああああああ
-
-// if (!isset($_COOKIE['id'])) {
-//   header("location:login.php");
-// }else {
+if (!isset($_COOKIE['id'])) {
+  header("location:login.php");
+}else {
 //データベースから全件取得
-$link = mysqli_connect(HOST, USER_ID, PASSWORD, DB_NAME);
-mysqli_set_charset($link, 'utf8');
-$result = mysqli_query($link, "SELECT id,username AS name FROM profile WHERE username = '" . $search . "';");
-$num_rows = mysqli_num_rows($result);
-if ($num_rows == 0) {
-  //何も取得できなかったら
-  $error = '';
-  $table[] = [
-    "id" => "",
-    "name" => ""
-  ];
-} else {
-  while ($row = mysqli_fetch_assoc($result)) {
-    $table[] = $row;
+  $link = mysqli_connect(HOST, USER_ID, PASSWORD, DB_NAME);
+  mysqli_set_charset($link, 'utf8');
+  $result = mysqli_query($link, "SELECT id,username AS name FROM profile WHERE username LIKE '%" . $search . "%';");
+  $num_rows = mysqli_num_rows($result);
+  if ($num_rows == 0) {
+    //何も取得できなかったら
+    $result = mysqli_query($link, "SELECT id,username AS name FROM profile;");
+    while ($row = mysqli_fetch_assoc($result)) {
+      $table[] = $row;
+    }
+  } else {
+    while ($row = mysqli_fetch_assoc($result)) {
+      $table[] = $row;
+    }
   }
+
+  mysqli_close($link);
+
 }
-
-mysqli_close($link);
-
-// }
 
 include './view/search.php';
 exit;
